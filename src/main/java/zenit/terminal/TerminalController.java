2666269c5633877ff264a87cdfad0c847f135409
omolander <oskar.molander@gmail.com>
2019-04-12 12:51:19 +0200

    TerminalFx now working

--- src/main/java/zenit/terminal/TerminalController.java
+++ src/main/java/zenit/terminal/TerminalController.java
@@ -1,51 +1,39 @@
 package main.java.zenit.terminal;
 
+import com.kodedu.terminalfx.TerminalBuilder;
+import com.kodedu.terminalfx.TerminalTab;
+import com.kodedu.terminalfx.config.TerminalConfig;
+
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-public class TerminalController extends Application {
+public class TerminalController {
 
-	@Override
-	public void start(Stage stage) throws Exception {
-		
-		
-		FXMLLoader loader = new FXMLLoader(getClass().getResource("Terminal.fxml"));
-		
-		Parent root = loader.load();
-		loader.setController(this);
-		loader.load();
-
-		Scene scene = new Scene(root);
-//		scene.getStylesheets().add(getClass().getResource("/zenit/ui/mainStyle.css").toString());
-//		scene.getStylesheets().add(getClass().getResource("/zenit/ui/keywords.css").toExternalForm());
-		stage.setScene(scene);
-		stage.setTitle("Zenit");
-
-		initialize();
-		stage.show();
-		
-	}
+	
+	@FXML
+	private TabPane tabPane;
 	
 	public void initialize() {
-		
 		addTerminalTab();
 	}
 	
-	private Tab addTerminalTab() {
+	private void addTerminalTab() {
+		TerminalConfig darkConfig = new TerminalConfig();
+		darkConfig.setBackgroundColor(Color.rgb(16, 16, 16));
+		darkConfig.setForegroundColor(Color.rgb(240, 240, 240));
+		darkConfig.setCursorColor(Color.rgb(255, 0, 0, 0.5));
 		
+		TerminalBuilder builder = new TerminalBuilder(darkConfig);
+		TerminalTab terminalTab = builder.newTerminal();
 		
-//		TerminalBuilder builder = new TerminalBuilder();
-//		TerminalTab terminalTab = new TerminalTab();
-		
-		return null;
-	}
-	
-	
-	public static void main(String[] args) {
-		launch(args);
+		tabPane.getTabs().add(terminalTab);
+
 	}
 }
